8b43cd84accc3366314aacc79194d529656e723d
chiefJurist <anthonynnanna004@gmail.com>
2026-06-19 08:55:23 +0100

    Configure Pusher broadcasting and register private channel authorization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+            //
+            $middleware->redirectGuestsTo(fn() => response()->json([
+                'message' => 'Unauthenticated.'
+            ], 401));
+        }
+    )
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
